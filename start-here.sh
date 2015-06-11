a54729bd88258a7fee8b67c681e3b051d24830b0
Jeffrey Aven <info@avensolutions.com>
2015-06-11 14:40:35 +1000

    updates

---
+++ start-here.sh
@@ -0,0 +1,12 @@
+###
+#
+# ansible-ec2-hadoop-cluster
+# Written by: Jeffrey Aven
+#             Aven Solutions Pty Ltd
+#             http://avensolutions.com
+#
+# Prerequisities:
+#    PEM File needs to be present in the Ansible control node users home directory
+#    Permissions need to be set to 400
+#
+###
